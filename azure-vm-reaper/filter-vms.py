63041deaa4ab94df00a1701bdf80cc89ba904e88
Kenaz <kenaz@puppetlabs.com>
2020-05-05 09:36:00 -0700

    Cleaning up filter-vms.py

--- azure-vm-reaper/filter-vms.py
+++ azure-vm-reaper/filter-vms.py
@@ -3,12 +3,12 @@
 # File: filter-vms.py 
 # Description: This is an example script that you can author or modify that retrieves 
 #              a list of Azure Virtual Machines from the Relay Interface (in the form of parameters)
-#              and filters the VMs that have no tags. It then sets the output
-#              variable `vms` to the list of Azure Virtual Machines that are untagged. 
+#              and filters the VMs that have no tags. It then sets the output variable `resource_ids` 
+#              to the list of Azure Virtual Machines resource IDs that are untagged. 
 # Inputs:
 #   - virtual_machines - list of Azure Virtual Machines
 # Outputs:
-#   - virtual_machines - list of Azure Virtual Machines to be terminated in the subsequent step
+#   - resource_ids - list of Azure Virtual Machine resource IDs to be terminated in the subsequent step
 
 import re
 
@@ -22,26 +22,26 @@ if __name__ == '__main__':
 
     # Filtering Azure virtual machines with no tags
     print('Looking for all Virtual Machines with no tags')
-    vms = ni.get(D.virtual_machines)
+    vms = ni.get(D.virtual_machines) # Queries for `virtual_machines` parameter from Relay
     for vm in vms:
         if 'tags' in vm.keys():
-            to_keep.append(vm)
+            to_keep.append(vm['id'])
             continue
         else:
             try:
-                to_terminate.append(vm)
+                to_terminate.append(vm['id'])
             except Exception as e:
                 print('\nAzure Virtual Machine {0} not considered for termination because of a processing error: {1}'.format(vm['name'], e))
     
-    print ('\nFound {} Virtual machines (with tags) not considered for termination:'.format(len(to_keep)))
-    print(*[vm['name'] for vm in to_keep], sep = "\n") 
+    print('\nFound {} Virtual machines (with tags) not considered for termination:'.format(len(to_keep)))
+    print(*[vm_id for vm_id in to_keep], sep = "\n") 
 
     if len(to_terminate) == 0:    
         print('\nNo Virtual Machines to terminate! Exiting.')
         exit()
     else:    
         print('\nAdding {} Virtual machines (without tags) to terminate:'.format(len(to_terminate)))
-        print(*[vm['name'] for vm in to_terminate], sep = "\n") 
-        print('\nSetting output `virtual_machines` to list of {0} virtual machines'.format(len(to_terminate)))
-        ni.outputs.set('virtual_machines', to_terminate)
-    
+        print(*[vm_id for vm_id in to_terminate], sep = "\n") 
+        print('\nSetting output `resource_ids` to list of {0} virtual machine resource ids to terminate:'.format(len(to_terminate)))
+        ni.outputs.set('resource_ids', to_terminate)
+        print(*[vm_id for vm_id in to_terminate], sep = "\n")
