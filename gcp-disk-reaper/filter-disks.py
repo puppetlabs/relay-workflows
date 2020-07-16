3a03ee0236edbfc8e6ec729e103dda6787cf5961
Travis Fields <1703710+cyberious@users.noreply.github.com>
2020-07-16 15:50:39 -0700

    Print disks to be reaped
    Show disks that are being added to the reaping.

--- gcp-disk-reaper/filter-disks.py
+++ gcp-disk-reaper/filter-disks.py
@@ -5,10 +5,11 @@ relay = Interface()
 
 if __name__ == '__main__':
     to_terminate = []
-
+    print('Disk to be terminated')
     disks = relay.get(D.disks)
     for disk in disks:
         if "users" not in disk.keys():
+            print(disk.get('name'))
             to_terminate.append(disk)
 
     print('Found {} disks that are unattached'.format(len(to_terminate)))
